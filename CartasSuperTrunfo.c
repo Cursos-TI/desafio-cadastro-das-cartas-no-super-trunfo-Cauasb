007264bdd6419788a19edd3143f11a40c913bb4c
Cauasb <cauajoaquim.desouzabezerra@gmail.com>
2025-08-06 22:39:26 +0000

    Finalizei o código de teste

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -4,9 +4,49 @@
 // Tema 1 - Cadastro das Cartas
 // Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
 // Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
+//Teste Joaquim
+
 
 int main() {
+
+    char estado;
+    char codigo[3];
+    char nomeCidade[30];
+    int populacao;
+    float area;
+    float pib;
+    int pontosTuristicos;
+
+    printf("Estado: ");
+    scanf("%c%*c", &estado);
+
+    printf("Codigo: ");
+    scanf("%s%*s", codigo);
+
+    printf("Nome da cidade: ");
+    scanf("%s", nomeCidade);
+
+    printf("Populacao: ");
+    scanf("%d", &populacao);
+
+    printf("Area: ");
+    scanf("%f", &area);
+
+    printf("PIB: ");
+    scanf("%f", &pib);
+
+    printf("Pontos turisticos: ");
+    scanf("%d", &pontosTuristicos);
+
+    printf("\nINFORMAÇÔES DA CARTA\n");
+    printf("Estado: %c\n", estado);
+    printf("Codigo: %s\n", codigo);
+    printf("Cidade: %s\n", nomeCidade);
+    printf("Populacao: %d\n", populacao);
+    printf("Area: %f\n", area);
+    printf("PIB: %f\n", pib);
+    printf("Pontos turisticos: %d\n", pontosTuristicos);
+
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
     
